8454b14c901d88ac6765165ff82e063065908cb5
Daniel Šmolík <dansmoliik@gmail.com>
2021-05-30 12:52:44 +0200

    Added comments to functions.

--- source/app.d
+++ source/app.d
@@ -30,124 +30,9 @@ enum string REPLY_OK = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n";
 
 string[MAX_EVENTS] replies;
 
-void add_new_communication_socket(int fd, int epollfd, epoll_event ev, ref sockaddr_in conn_addr, ref socklen_t addr_size)
-{
-    int conn_sock = accept( fd, cast(sockaddr *) &conn_addr, &addr_size);
-    if (conn_sock == -1) {
-        perror( "accept");
-        exit( EXIT_FAILURE);
-    }
-    debug writeln( "SERVER: Got connection from: ", inet_ntoa( conn_addr.sin_addr), ":", ntohs( conn_addr.sin_port));
-
-    if (fcntl( conn_sock, F_SETFL, fcntl( conn_sock, F_GETFL, 0) | O_NONBLOCK) == -1){
-        perror( "calling fcntl");
-        exit( EXIT_FAILURE);
-    }
-    ev.events = EPOLLIN;
-    ev.data.fd = conn_sock;
-    if (epoll_ctl( epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1) {
-        perror( "epoll_ctl: add conn_sock");
-        exit( EXIT_FAILURE);
-    }
-}
-
-ssize_t recv_msg(int fd, ref string msg)
-{
-    ssize_t total_size = 0;
-    ssize_t size_recv;
-    char *chunk = cast(char*)malloc( READ_CHUNK_SIZE);
-    msg = "";
-
-    while ((size_recv =  recv( fd, chunk, READ_CHUNK_SIZE, 0) ) > 0)
-    {
-        total_size += size_recv;
-        msg ~= to!string( chunk);
-        //debug writeln( "\t", to!string( chunk));
-        memset( chunk , 0, READ_CHUNK_SIZE);    //clear the variable chunk
-    }
-    return total_size;
-}
-
-string create_json(string uri)
-{
-    ptrdiff_t index_of_name_substr = indexOf( uri, "name"); // find parameter "name" in the uri
-    if (index_of_name_substr == -1 || index_of_name_substr + 4 >= uri.length || uri[index_of_name_substr + 4] != '=')
-        return "{}\r\n"; // there is no parameter "name"
-
-    string data_substr = uri[index_of_name_substr + 5..$]; // slice off everything before actual name
-    ptrdiff_t index_of_symbol_and = indexOf( data_substr, "&"); // search for other parameters
-
-    if (index_of_symbol_and == -1)
-        return "{\"hello\":\"" ~ data_substr ~ "\"}\r\n"; // "name" is the only parameter
-    return "{\"hello\":\"" ~ data_substr[0..index_of_symbol_and] ~ "\"}\r\n"; // slice off other parameters
-}
-
-string create_reply_ok(string uri)
-{
-    string json_str = create_json( uri);
-    return REPLY_OK ~ "Content-Length: " ~ to!string( json_str.length) ~ "\r\n\r\n" ~ json_str;
-}
-
-string create_reply_from_msg(string msg)
-{
-    string reply;
-    auto parser = initParser!Msg();
-    parser.parseRequest( msg);
-    debug writeln( "SERVER:\tCreating response to method: ", parser.method);
-
-    switch (parser.method)
-    {
-        case "GET":
-        return create_reply_ok( to!string( parser.uri));
-        default:
-        return REPLY_NOT_FOUND;
-    }
-}
-
-void save_reply(int fd, string reply)
-{
-    replies[fd + FDS_ARR_OFFSET] = reply;
-    debug writeln( "SERVER:\tReply saved at index: ", fd + FDS_ARR_OFFSET);
-}
-
-void receive_msg(int fd, int epollfd, epoll_event ev)
-{
-    ssize_t msg_size;
-    string msg, reply;
-
-    debug writeln( "SERVER:\tReceiving message from: ", fd);
-
-    msg_size = recv_msg( fd, msg);
-    debug writeln( "SERVER:\tTotal msg length:", msg_size);
-    debug writeln( "SERVER:\tMessage received: ", msg);
-
-    reply = create_reply_from_msg( msg);
-    debug writeln( "SERVER:\tCreated reply: ", reply);
-
-    save_reply( fd, reply); // reply is saved here and it is sent later
-    //Set Write Action Events for Annotation
-    ev.data.fd = fd;
-    ev.events=EPOLLOUT;
-    epoll_ctl( epollfd,EPOLL_CTL_MOD, fd, &ev);
-}
-
-void send_reply(int fd, int epollfd, epoll_event ev)
-{
-
-    string reply = replies[fd + FDS_ARR_OFFSET];
-    debug writeln( "SERVER:\tSending reply: ", reply);
-
-    send( fd, cast(char*)reply, reply.length, 0);
-
-    ev.data.fd = fd;
-    if (epoll_ctl( epollfd, EPOLL_CTL_DEL, fd, &ev) == -1) {
-        perror( "epoll_ctl: del conn_sock");
-        exit( EXIT_FAILURE);
-    }
-    close( fd);
-    debug writeln( "SERVER:\tClosed: :", fd);
-}
-
+/*
+ * Initialize server - listening socket and epoll.
+*/
 void init_server(ref int listen_sock, ref int epollfd, ref epoll_event ev, ref epoll_event* events)
 {
     events = cast(epoll_event*)malloc( MAX_EVENTS * epoll_event.sizeof);
@@ -193,6 +78,149 @@ void init_server(ref int listen_sock, ref int epollfd, ref epoll_event ev, ref e
     }
 }
 
+/*
+ * Function responsible for accepting new communication and adding it to epoll.
+*/
+void add_new_communication_socket(int fd, int epollfd, epoll_event ev, ref sockaddr_in conn_addr, ref socklen_t addr_size)
+{
+    int conn_sock = accept( fd, cast(sockaddr *) &conn_addr, &addr_size);
+    if (conn_sock == -1) {
+        perror( "accept");
+        exit( EXIT_FAILURE);
+    }
+    debug writeln( "SERVER: Got connection from: ", inet_ntoa( conn_addr.sin_addr), ":", ntohs( conn_addr.sin_port));
+
+    if (fcntl( conn_sock, F_SETFL, fcntl( conn_sock, F_GETFL, 0) | O_NONBLOCK) == -1){
+        perror( "calling fcntl");
+        exit( EXIT_FAILURE);
+    }
+    ev.events = EPOLLIN;
+    ev.data.fd = conn_sock;
+    if (epoll_ctl( epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1) {
+        perror( "epoll_ctl: add conn_sock");
+        exit( EXIT_FAILURE);
+    }
+}
+
+/*
+ * Function responsible for reading a whole message from a socket.
+*/
+ssize_t recv_msg(int fd, ref string msg)
+{
+    ssize_t total_size = 0;
+    ssize_t size_recv;
+    char *chunk = cast(char*)malloc( READ_CHUNK_SIZE);
+    msg = "";
+
+    while ((size_recv =  recv( fd, chunk, READ_CHUNK_SIZE, 0) ) > 0)
+    {
+        total_size += size_recv;
+        msg ~= to!string( chunk);
+        //debug writeln( "\t", to!string( chunk));
+        memset( chunk , 0, READ_CHUNK_SIZE);    //clear the variable chunk
+    }
+    return total_size;
+}
+
+/*
+ * Function responsible for creating json reply string based on request uri.
+ * If there is a parameter "name", it returns string in format of "{"hello": "<name>"}", otherwise "{}"
+*/
+string create_json(string uri)
+{
+    ptrdiff_t index_of_name_substr = indexOf( uri, "name"); // find parameter "name" in the uri
+    if (index_of_name_substr == -1 || index_of_name_substr + 4 >= uri.length || uri[index_of_name_substr + 4] != '=')
+        return "{}\r\n"; // there is no parameter "name"
+
+    string data_substr = uri[index_of_name_substr + 5..$]; // slice off everything before actual name
+    ptrdiff_t index_of_symbol_and = indexOf( data_substr, "&"); // search for other parameters
+
+    if (index_of_symbol_and == -1)
+        return "{\"hello\":\"" ~ data_substr ~ "\"}\r\n"; // "name" is the only parameter
+    return "{\"hello\":\"" ~ data_substr[0..index_of_symbol_and] ~ "\"}\r\n"; // slice off other parameters
+}
+
+/*
+ * Create a reply to GET request.
+*/
+string create_reply_ok(string uri)
+{
+    string json_str = create_json( uri);
+    return REPLY_OK ~ "Content-Length: " ~ to!string( json_str.length) ~ "\r\n\r\n" ~ json_str;
+}
+
+/*
+ * Create a reply based on the "msg" method and uri.
+*/
+string create_reply_from_msg(string msg)
+{
+    string reply;
+    auto parser = initParser!Msg();
+    parser.parseRequest( msg);
+    debug writeln( "SERVER:\tCreating response to method: ", parser.method);
+
+    switch (parser.method)
+    {
+        case "GET":
+        return create_reply_ok( to!string( parser.uri));
+        default:
+        return REPLY_NOT_FOUND;
+    }
+}
+
+/*
+ * Save a reply to array of replies so it can be later sent.
+*/
+void save_reply(int fd, string reply)
+{
+    replies[fd + FDS_ARR_OFFSET] = reply;
+    debug writeln( "SERVER:\tReply saved at index: ", fd + FDS_ARR_OFFSET);
+}
+
+/*
+ * The main function responsible for reading a message and creating a reply to it.
+*/
+void receive_msg(int fd, int epollfd, epoll_event ev)
+{
+    ssize_t msg_size;
+    string msg, reply;
+
+    debug writeln( "SERVER:\tReceiving message from: ", fd);
+
+    msg_size = recv_msg( fd, msg);
+    debug writeln( "SERVER:\tTotal msg length:", msg_size);
+    debug writeln( "SERVER:\tMessage received: ", msg);
+
+    reply = create_reply_from_msg( msg);
+    debug writeln( "SERVER:\tCreated reply: ", reply);
+
+    save_reply( fd, reply); // reply is saved here and it is sent later
+    //Set Write Action Events for Annotation
+    ev.data.fd = fd;
+    ev.events=EPOLLOUT;
+    epoll_ctl( epollfd,EPOLL_CTL_MOD, fd, &ev);
+}
+
+/*
+ * Function responsible for sending a reply.
+*/
+void send_reply(int fd, int epollfd, epoll_event ev)
+{
+
+    string reply = replies[fd + FDS_ARR_OFFSET];
+    debug writeln( "SERVER:\tSending reply: ", reply);
+
+    send( fd, cast(char*)reply, reply.length, 0);
+
+    ev.data.fd = fd;
+    if (epoll_ctl( epollfd, EPOLL_CTL_DEL, fd, &ev) == -1) {
+        perror( "epoll_ctl: del conn_sock");
+        exit( EXIT_FAILURE);
+    }
+    close( fd);
+    debug writeln( "SERVER:\tClosed: :", fd);
+}
+
 int main()
 {
     writeln( "SERVER: SERVER STARTED");
@@ -203,8 +231,8 @@ int main()
     epoll_event ev;
     epoll_event* events;
 
-    init_server(listen_sock, epollfd, ev, events);
-    debug writeln("SERVER: INITIALIZED");
+    init_server( listen_sock, epollfd, ev, events);
+    debug writeln( "SERVER: INITIALIZED");
 
     for (;;)
     {
@@ -220,8 +248,8 @@ int main()
         {
             if (events[n].data.fd == listen_sock)
             {
-                debug writeln("SERVER: NEW CONNECTION");
-                add_new_communication_socket(listen_sock, epollfd, ev, conn_addr, addr_size);
+                debug writeln( "SERVER: NEW CONNECTION");
+                add_new_communication_socket( listen_sock, epollfd, ev, conn_addr, addr_size);
             } else if (events[n].events&EPOLLIN)
             {
                 debug writeln( "SERVER: EPOLLIN ",events[n].data.fd);
